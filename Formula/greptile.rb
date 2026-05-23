da48b6f1cf69f0965ba4ebd49a5d5d39af50a5e8
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-05-22 21:35:29 -0400

    Update greptile to v3.0.4

--- Formula/greptile.rb
+++ Formula/greptile.rb
@@ -3,9 +3,9 @@
 class Greptile < Formula
   desc "AI code review from your terminal"
   homepage "https://greptile.com"
-  url "https://github.com/greptileai/cli/releases/download/v3.0.3/greptile.js"
-  version "3.0.3"
-  sha256 "e60deeb55597ea5b0b118ef23c1ab07f134d191f912b30af7e2010d27b6d7d01"
+  url "https://github.com/greptileai/cli/releases/download/v3.0.4/greptile.js"
+  version "3.0.4"
+  sha256 "55a4cdcef0b577bc9388b24eca34a5aa0f5f439104e0c44fc8dbf9331a63fdad"
   license "MIT"
 
   depends_on "node"
