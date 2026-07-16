3b65dde6e308c7d8125091c68f4e9ef0c20628a5
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-07-15 18:14:35 -0700

    fix greptile 3.2.1 audit

--- Formula/greptile.rb
+++ Formula/greptile.rb
@@ -4,7 +4,6 @@ class Greptile < Formula
   desc "AI code review from your terminal"
   homepage "https://greptile.com"
   url "https://github.com/greptileai/cli/releases/download/v3.2.1/greptile.js"
-  version "3.2.1"
   sha256 "d5640427c27efff9ff3f3e5194153078038b1e763a9aa3bb8f9909e6cb5958e8"
   license "MIT"
 
