ee6812b719a9e93b9e0cb12c8cc4a6d28c0ca649
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2026-08-10 19:57:03 -0700

    chore: release greptile 3.4.0

--- Formula/greptile.rb
+++ Formula/greptile.rb
@@ -3,8 +3,8 @@
 class Greptile < Formula
   desc "AI code review from your terminal"
   homepage "https://greptile.com"
-  url "https://github.com/greptileai/cli/releases/download/v3.3.1/greptile.js"
-  sha256 "def9649410d31cc15dfb7e4aef9fc2f11cd90deff9232caa499059aa1d6af3a7"
+  url "https://github.com/greptileai/cli/releases/download/v3.4.0/greptile.js"
+  sha256 "3dff8d68be7872515cb879bc7085e58c3a15cacff600cc87d29c6dd5d0e48fed"
   license "MIT"
 
   depends_on "node"
